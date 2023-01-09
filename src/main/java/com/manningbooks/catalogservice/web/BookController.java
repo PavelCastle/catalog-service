845299c606878c905b3ecd48a6cd9f07f14a5b66
crpavel22 <crpavel@icloud.com>
2023-01-09 16:02:05 -0600

    Added log messages

--- src/main/java/com/manningbooks/catalogservice/web/BookController.java
+++ src/main/java/com/manningbooks/catalogservice/web/BookController.java
@@ -3,6 +3,8 @@ package com.manningbooks.catalogservice.web;
 import com.manningbooks.catalogservice.domain.Book;
 import com.manningbooks.catalogservice.domain.BookService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("books")
 public class BookController {
 
+    public static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -26,28 +30,33 @@ public class BookController {
 
     @GetMapping
     public Iterable<Book> get() {
+        log.info("Fetching the list of books in the catalog.");
         return bookService.viewBookList();
     }
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info("Fetching a book form catalog with is isbn: {}", isbn);
         return bookService.viewBookDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book post(@Valid @RequestBody Book book) {
+        log.info("Creating a book in catalog: {}", book);
         return bookService.addBookToCatalog(book);
     }
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("Removing the book with ISBN: {}", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
     public Book put(@PathVariable("isbn") String isbn, @Valid @RequestBody Book book) {
+        log.info("Updating the book {} with this new values: {}", isbn, book);
         return bookService.editBookDetails(isbn, book);
     }
 }
