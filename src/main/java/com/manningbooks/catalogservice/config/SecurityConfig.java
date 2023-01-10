49c3836008522ba7796aaf7f08521dd7a6efc4a2
crpavel22 <crpavel@icloud.com>
2023-01-09 20:08:00 -0600

    Enabled health endpoints from actauator

--- src/main/java/com/manningbooks/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/manningbooks/catalogservice/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
                         .anyRequest().hasRole("employee")
                 )
